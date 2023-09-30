6fe94fdc2c53712f521cbaf6ed431fd911fbb10f
Yefer YV <yucrayeff36@gmail.com>
2023-09-30 13:40:21 -0500

    fix(lf): imgcat cleaner with %6

--- AppData/Local/lf/lf_clean.sh
+++ AppData/Local/lf/lf_clean.sh
@@ -3,5 +3,7 @@
 # grep $(basename "$1") ~/.cache/lf_clean || lf -remote "send $ID redraw"
 # basename "$1" > ~/.cache/lf_clean
 
-[[ "$( basename "$1")" != "$(cat ~/.cache/LF_USER_F)" ]] && lf -remote "send $ID redraw"
-basename "$1" > ~/.cache/LF_USER_F
+# [[ "$( basename "$1")" != "$(cat ~/.cache/LF_USER_F)" ]] && lf -remote "send $ID redraw"
+# basename "$1" > ~/.cache/LF_USER_F
+
+[[ "$( basename "$1")" != "$( basename "$6")" ]] && lf -remote "send $ID redraw"
