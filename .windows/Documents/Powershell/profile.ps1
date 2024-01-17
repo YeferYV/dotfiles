5f1f15bad79bdd3228cd4cda5038a65142e04d4d
Yefer YV <yucrayeff36@gmail.com>
2024-01-17 00:09:57 -0500

    feat(.windows): powershell version 5 support

--- .windows/Documents/Powershell/profile.ps1
+++ .windows/Documents/Powershell/profile.ps1
@@ -1,51 +1,47 @@
-# FAST-WINRICING:
-# curl -L get.scoop.sh | pwsh
-# scoop install wezterm-nightly lf neovim touchcursor git
-# git clonw https://github.com/nvchad/nvchad $HOME/AppData/Local/nvim
-# touchcursor
+# install ReadLine in powershell 5:
+# Install-Module -Name PowerShellGet -Force
+# Install-Module PSReadLine -AllowPrerelease -Force
 
 # Change working dir in powershell to last dir in lf on exit.
 # You need to put this file to a folder in $ENV:PATH variable.
 # You may also like to assign a key to this command:
 #
-    Set-PSReadLineOption -EditMode Vi
-    Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -EditMode Vi
+Set-PSReadLineOption -PredictionSource History  # Readline Required
 
-    Set-PSReadLineKeyHandler -Chord Alt+o -ScriptBlock {
-        [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-        #[Microsoft.PowerShell.PSConsoleReadLine]::Insert('lfcd.ps1')
-        [Microsoft.PowerShell.PSConsoleReadLine]::Insert('lfcd')
-        [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-    }
+Set-PSReadLineKeyHandler -Chord Alt+o -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+    #[Microsoft.PowerShell.PSConsoleReadLine]::Insert('lfcd.ps1')
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert('lfcd')
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+}
 
-    Set-PSReadLineKeyHandler -Chord Alt+y -ScriptBlock {
-        [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-        [Microsoft.PowerShell.PSConsoleReadLine]::Insert('y')
-        [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-    }
+Set-PSReadLineKeyHandler -Chord Alt+y -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert('y')
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+}
 
-    Set-PSReadlineKeyHandler -Chord Alt+h -Function ViBackwardChar -ViMode Command
-    Set-PSReadlineKeyHandler -Chord Alt+h -Function ViCommandMode -ViMode Insert
+Set-PSReadlineKeyHandler -Chord Alt+h -Function ViBackwardChar -ViMode Command # Readline required
+Set-PSReadlineKeyHandler -Chord Alt+h -Function ViCommandMode -ViMode Insert   # Readline required
 
-    Set-PSReadlineKeyHandler -Chord Alt+j -Function HistorySearchForward -ViMode Command
-    Set-PSReadLineKeyHandler -Chord Alt+j -ScriptBlock {
-        [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchForward()
-        [Microsoft.PowerShell.PSConsoleReadLine]::ViCommandMode()
-    }
+Set-PSReadlineKeyHandler -Chord Alt+j -Function HistorySearchForward -ViMode Command
+Set-PSReadLineKeyHandler -Chord Alt+j -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchForward()
+    [Microsoft.PowerShell.PSConsoleReadLine]::ViCommandMode()
+}
 
-    Set-PSReadlineKeyHandler -Chord Alt+k -Function HistorySearchBackward -ViMode Command
-    Set-PSReadLineKeyHandler -Chord Alt+k -ScriptBlock {
-        [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchBackward()
-        [Microsoft.PowerShell.PSConsoleReadLine]::ViCommandMode()
-    }
+Set-PSReadlineKeyHandler -Chord Alt+k -Function HistorySearchBackward -ViMode Command
+Set-PSReadLineKeyHandler -Chord Alt+k -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchBackward()
+    [Microsoft.PowerShell.PSConsoleReadLine]::ViCommandMode()
+}
 
-    Set-PSReadlineKeyHandler -Chord Alt+l -Function ViForwardChar -ViMode Command
-    Set-PSReadLineKeyHandler -Chord Alt+l -ScriptBlock {
-        [Microsoft.PowerShell.PSConsoleReadLine]::AcceptSuggestion()
-        [Microsoft.PowerShell.PSConsoleReadLine]::ViCommandMode()
-    }
-
-
+Set-PSReadlineKeyHandler -Chord Alt+l -Function ViForwardChar -ViMode Command  # Readline required
+Set-PSReadLineKeyHandler -Chord Alt+l -ScriptBlock {                           # Readline required
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptSuggestion()
+    [Microsoft.PowerShell.PSConsoleReadLine]::ViCommandMode()
+}
 
 # Shows navigable menu of all options when hitting Tab
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
@@ -58,17 +54,8 @@ Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 # You may put this in one of the profiles found in $PROFILE.
 #
 function lfcd {
-	$tmp = [System.IO.Path]::GetTempFileName()
-	lf -last-dir-path="$tmp" $args
-	if (test-path -pathtype leaf "$tmp") {
-	      $dir = type "$tmp"
-		  remove-item -force "$tmp"
-		  if (test-path -pathtype container "$dir") {
-		    if ("$dir" -ne "$pwd") {
-				  cd "$dir"
-		    }
-		  }
-	}
+	lf -print-last-dir $args | Set-Location
+  printf "\x1b[A\x1b[K";
 }
 
 function y() {
@@ -103,27 +90,18 @@ $env:OPENER="start"
 # $env:PATHEXT=".COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC;.CPL;.SH"
 $env:XDG_CONFIG_HOME="$HOME/.config"
 # $env:MPV_HOME="$HOME\.config\mpv"
-$env:PATH+=";$HOME\.cargo\bin"
+# $env:PATH+=";$HOME\.cargo\bin"
 $XDG_CONFIG_HOME="$HOME/.config"
-
-Function lf_dir {
-  lf -command 'source C:\\Users\\yeste\\.config\\lf\\lfrc'
-}
-
-Set-Alias -Name lfdir -Value lf_dir
-
-# function Do-ActualThing {
-#     nvim
-# }
-# Set-Alias n Do-ActualThing
 
 Set-Alias n nvim
 Set-Alias ll ls
 
 # pwsh prompt
-starship config format "(`$battery)(`$sudo)(`$username)(`$directory)(`$git_branch)(`$git_status)(`$cmd_duration)(`$status)(`$character)"
-starship config add_newline false
-starship config cmd_duration.min_time 60000
-starship config directory.style "bold fg:#5555cc"
-starship config directory.truncation_length 9
+if (-not (Test-Path "$HOME/.config/starship.toml")) {
+  starship config format "(`$battery)(`$sudo)(`$username)(`$directory)(`$git_branch)(`$git_status)(`$cmd_duration)(`$status)(`$character)"
+  starship config add_newline false
+  starship config cmd_duration.min_time 60000
+  starship config directory.style "bold fg:#5555cc"
+  starship config directory.truncation_length 9
+}
 Invoke-Expression (&starship init powershell)
